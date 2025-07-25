7b63fb91d2a76c02de2cf6ed1ac528a79ad8f14c
Gugan <110531967+kokkigugan@users.noreply.github.com>
2025-07-25 08:36:14 +0530

    1 question completed

---
+++ 1b.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name :");
+        string? name =Console.ReadLine();
+        Console.WriteLine($"Your name {name}");
+    }
+}
---
+++ 1c.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the Data:");
+        int? age =Convert.ToInt32(Console.ReadLine());
+        string? name = Console.ReadLine();
+        double? height = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"Hello, {name}! You are {age} years old and {height} cm tall.");
+    }
+}
